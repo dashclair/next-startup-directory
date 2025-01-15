69953fd4d798dd24f5c76d64ef2fad36952b68f4
dasha <darya.gapeeva.01@gmail.com>
2025-01-15 13:20:39 +0100

    feat: create a startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,76 @@
+import Image from "next/image";
 import SearchForm from "../components/SearchForm";
+import StartupCard from "../components/StartupCard";
+
+export interface StartupCardProps {
+  _createdAt?: Date;
+  title: string;
+  description: string;
+  image?: string;
+  views: number;
+  _id: number;
+}
+
+const posts = [
+  {
+    _createdAt: new Date(),
+    views: 30,
+    author: { _id: 1 },
+    _id: 1,
+    description:
+      "A mobile app that helps users track and reduce their carbo and best ins...",
+    image:
+      "https://img.freepik.com/premium-photo/toys-against-pink-background_1048944-22938314.jpg?w=826",
+    category: "Ecology",
+    title: "EcoTrack",
+  },
+  {
+    _createdAt: new Date(),
+    views: 30,
+    author: { _id: 2, name: "Adrian" },
+    _id: 2,
+    description:
+      "A mobile app that helps users track and reduce their carbo and best ins...",
+    image:
+      "https://img.freepik.com/premium-photo/toys-against-pink-background_1048944-22938314.jpg?w=826",
+    category: "Ecology",
+    title: "EcoTrack",
+  },
+  {
+    _createdAt: new Date(),
+    views: 30,
+    author: { _id: 3, name: "Adrian" },
+    _id: 3,
+    description:
+      "A mobile app that helps users track and reduce their carbo and best ins...",
+    image:
+      "https://img.freepik.com/premium-photo/toys-against-pink-background_1048944-22938314.jpg?w=826",
+    category: "Ecology",
+    title: "EcoTrack",
+  },
+  {
+    _createdAt: new Date(),
+    views: 30,
+    author: { _id: 4, name: "Adrian" },
+    _id: 4,
+    description:
+      "A mobile app that helps users track and reduce their carbo and best ins...",
+    image:
+      "https://img.freepik.com/premium-photo/toys-against-pink-background_1048944-22938314.jpg?w=826",
+    category: "Ecology",
+    title: "EcoTrack",
+  },
+];
+
+export type StartupPost = (typeof posts)[number];
 
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
-  const query = (await searchParams).query as string
+  const query = (await searchParams).query as string;
+
   return (
     <>
       <section className="pink-heading">
@@ -19,6 +84,20 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="py-10 px-6">
+        <p className="text-2xl font-sans font-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupPost) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p>no posts found</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ libs/formatDate.ts
@@ -0,0 +1,8 @@
+
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-us", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+};
